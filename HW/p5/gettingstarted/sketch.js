a8d602d72b64f56b06a675e403c62d0148fe7d88
Amanda L'Hussier <amandalhussier@Amandas-MacBook-Air-3.local>
2018-02-01 14:39:50 -0500

    2/1/18

--- HW/p5/gettingstarted/sketch.js
+++ HW/p5/gettingstarted/sketch.js
@@ -1,25 +1,59 @@
 var centerX = 200;
 var centerY = 200;
+var backgroundColor = "white";
+
+var button;
+var button2;
+
+var noseColor = "black";
+
+var sel;
+var eyeColor = "black";
 
 function setup() {
   // put setup code here
   createCanvas(400,400);
   background(255);
+  button = createButton("Click Me");
+  button.mousePressed(changeBG);
+  button2 = createButton("Red");
+  button2.mousePressed(function(){
+
+    //your code goes here
+    noseColor = "red";
+
+  });
+ //below is how to creat a drop down selection with eye color options
+  sel = createSelect();
+  sel.option("blue");
+  sel.option("green");
+  sel.option("brown");
+
+  sel.changed(function(){
+//these are how to change the eyes to the selected color value 
+    eyeColor = sel.value();
+
+  });
 }
 
 function draw() {
   // put drawing code here
 
-  background(255);
+  background(backgroundColor);
   centerX = mouseX;
   centerY = mouseY;
   stroke(0);
   strokeWeight(1);
-  fill("white");
+  fill(eyeColor);
   rect(centerX-150,centerY-100,100,100);
-  ellipse(centerX-100,centerY-50,50,50);
+  //eye1
+  var xOffset = map(mouseX, 0, width, -20,20);
+  var yOffset = map(mouseY, 0, height, -20,20);
+  console.log("xOffset: " + xOffset + "yOffset: " + yOffset);
+  ellipse(centerX-100 + xOffset, centerY-50 +yOffset, 50,50);
   rect(centerX+25,centerY-100,100,100);
-  ellipse(centerX+75,centerY-50,50,75);
+  //eye2
+  ellipse(centerX+75 + xOffset,centerY-50 +yOffset,50,75);
 
   stroke(125);
   strokeWeight(5);
@@ -30,8 +64,21 @@ function draw() {
   line(centerX-50,centerY+50,centerX+50,centerY+50);
 
   noStroke();
-  fill(0,0,255);
+  fill(noseColor);
+  //nose
   triangle(centerX,centerY,centerX+25,centerY+25,centerX-25,centerY+25);
 
 
+}
+
+function mousePressed(){
+  backgroundColor = "blue";
+}
+
+function mouseReleased(){
+  backgroundColor = "red";
+}
+
+function changeBG(){
+  noseColor = "green";
 }
