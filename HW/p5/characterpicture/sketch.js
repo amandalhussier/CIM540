1502b786ee80522565d58b421d622f26b5a6a6d0
Amanda L'Hussier <amandalhussier@Amandas-MacBook-Air-3.local>
2018-02-01 01:52:35 -0500

    character progress

--- HW/p5/characterpicture/sketch.js
+++ HW/p5/characterpicture/sketch.js
@@ -1,26 +1,45 @@
-var centerX = 100
-var centerY = 100
+var centerX = 200
+var centerY = 200
 
 
 
 function setup() {
   createCanvas (600,600);
-  background (300);
+  background (230);
 }
 
 function draw() {
-  background (300);
-  ellipse (140, 0, 190, 190);
-// The rectangle draws on top of the ellipse
-// because it comes after in the code
-	    rect (160, 30, 260, 20);
-      
-  	 background (0);		// Black
-	   fill (204);			// Light Gray
-	   ellipse (132, 82, 200, 200);	// Light Gray Circle
-	   fill (153);			// Medium Gray
-	   ellipse (228, -16, 200, 200);	// Medium Gray Circle
-	   fill (102);			// Dark Gray
-  	 ellipse (268, 118, 200, 200);	// Dark Gray Circle
 
+  	 background (0);		// Black
+
+     //body
+     noStroke();
+	   fill (204);			// Light Gray
+	   ellipse (centerX, 110, 200, 200);	// Light Gray Circle
+	   fill (153);			// Medium Gray
+	   ellipse (centerY +20, centerX, 200, 200);	// Medium Gray Circle
+	   fill (102);			// Dark Gray
+  	 ellipse (centerY + 90, centerX + 100, 200, 200);	// Dark Gray Circle
+     fill (92);			// Darkest Gray
+  	 ellipse (centerY + 190, centerX + 140, 200, 200);	// Darkest Gray Circle
+
+     //antennae
+     line(centerY + 200, centerX + 90, 320, 200); //left
+     stroke(255);
+     line(centerY + 200, centerX + 90, 380, 200); //middle
+     stroke(255);
+     line(centerY + 200, centerX + 90, 440, 200); //right
+     stroke(255);
+
+     //eyes
+     ellipse (centerY + 230, centerX + 130, 70, 65); //right eye
+     ellipse (centerY + 250, centerX + 130, 25, 25) //right pupil
+     ellipse (centerY + 170, centerX + 130, 70, 65) //left eye
+     ellipse (centerY + 190, centerX + 130, 25, 25) //left pupil
+     strokeWeight(10)
+
+     //mouth
+     stroke(255,0,0);
+     line(340, 390, 430, 400);
+     strokeWeight(6)
 }
