fbff0866e3bb434565dc65973c756fdfc61a1cd8
Amanda L'Hussier <amandalhussier@Amandas-MacBook-Air-3.local>
2018-02-01 00:24:52 -0500

    start of character building

--- HW/p5/characterpicture/sketch.js
+++ HW/p5/characterpicture/sketch.js
@@ -1,7 +1,27 @@
+var centerX = 100
+var centerY = 100
+
+
+
 function setup() {
-  // put setup code here
+  createCanvas (600,600);
+  background (300);
 }
 
 function draw() {
-  // put drawing code here
+  background (300);
+  ellipse (140, 0, 190, 190);
+// The rectangle draws on top of the ellipse
+// because it comes after in the code
+	    rect (160, 30, 260, 20);
+	}
+  	   background (0);		// Black
+	   fill (204);			// Light Gray
+	   ellipse (132, 82, 200, 200);	// Light Gray Circle
+	   fill (153);			// Medium Gray
+	   ellipse (228, -16, 200, 200);	// Medium Gray Circle
+	   fill (102);			// Dark Gray
+  	   ellipse (268, 118, 200, 200);	// Dark Gray Circle
+	}
+
 }
