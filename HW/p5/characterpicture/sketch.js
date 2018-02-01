8505511f116c5b1c726cc4423b1e21b4674459f2
Amanda L'Hussier <amandalhussier@Amandas-MacBook-Air-3.local>
2018-02-01 00:36:26 -0500

    re adding text

--- HW/p5/characterpicture/sketch.js
+++ HW/p5/characterpicture/sketch.js
@@ -14,14 +14,13 @@ function draw() {
 // The rectangle draws on top of the ellipse
 // because it comes after in the code
 	    rect (160, 30, 260, 20);
-	}
-  	   background (0);		// Black
+      
+  	 background (0);		// Black
 	   fill (204);			// Light Gray
 	   ellipse (132, 82, 200, 200);	// Light Gray Circle
 	   fill (153);			// Medium Gray
 	   ellipse (228, -16, 200, 200);	// Medium Gray Circle
 	   fill (102);			// Dark Gray
-  	   ellipse (268, 118, 200, 200);	// Dark Gray Circle
-	}
+  	 ellipse (268, 118, 200, 200);	// Dark Gray Circle
 
 }
